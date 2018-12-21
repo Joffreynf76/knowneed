983a3b9d7fa3544f191c0c9f0488d791419a7b51
joffrey <joffrey-lhermitte@hotmail.fr>
2018-12-21 12:00:40 +0100

    final commit

--- profil.php
+++ profil.php
@@ -231,7 +231,7 @@ include 'header.php';
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modifier mes informations</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel">Add description</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
